0a75cb4504c8d3ea9fc85d6c523dc4e974b03fe7
CherryQuartzio <quaminoquang@gmail.com>
2025-07-20 00:09:41 +0700

    Implemented condition checking for when envycontrol should not run

--- ui/AttachedToBatteryView.js
+++ ui/AttachedToBatteryView.js
@@ -5,50 +5,52 @@ import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js'
 
 import * as Utility from '../lib/Utility.js';
 
-
 export const AttachedToBatteryToggle = GObject.registerClass(
-class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {
+class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {  
     _init(extensionObject) {
-        const currentProfile = Utility.getCurrentProfile();
+        this.activeProfile = Utility.getCurrentProfile(); // active profile since startup
+        this.requestedProfile = null;
+        this.chosenProfile = this.activeProfile === Utility.GPU_PROFILE_UNKNOWN // currently selected profile
+                ? 'unknown'
+                : this.activeProfile;
+        this.restartPending = false;
+        
         super._init({
             title: 'GPU Profile',
-            subtitle: currentProfile === Utility.GPU_PROFILE_UNKNOWN
-                ? 'Unknown'
-                : Utility.capitalizeFirstLetter(currentProfile),
+            subtitle: Utility.capitalizeFirstLetter(this.chosenProfile),
             iconName: 'power-profile-performance-symbolic',
             toggleMode: false, // disable the possibility to click the button
-            checked: currentProfile === 'hybrid' || currentProfile === 'nvidia',
+            checked: this.activeProfile === 'hybrid' || this.activeProfile === 'nvidia',
         });
         this.all_settings = extensionObject.getSettings();
 
-        const headerTitle = currentProfile === Utility.GPU_PROFILE_UNKNOWN
-            ? 'Select GPU Profile'
-            : Utility.capitalizeFirstLetter(currentProfile);
-
         // This function is unique to this class. It adds a nice header with an icon, title and optional subtitle.
-        this.menu.setHeader('power-profile-performance-symbolic', headerTitle, 'Choose a GPU mode');
+        this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Choose a GPU mode');
 
         // add a sections of items to the menu
         this._itemsSection = new PopupMenu.PopupMenuSection();
-        this._itemsSection.addAction('Integrated', () => {
-            if (currentProfile !== 'integrated') {
-                Utility.switchIntegrated();
-                super.subtitle = 'Integrated';
-                this.menu.setHeader('power-profile-performance-symbolic', headerTitle + ' → Integrated', 'Restart to apply');
+        this._itemsSection.addAction('Integrated' + (this.activeProfile === 'integrated' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'integrated' && this.requestedProfile === null) {
+                this.requestedProfile = 'integrated';
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Integrated mode...');
+                Utility.switchIntegrated(this._onSwitchComplete.bind(this));
             }
         });
-        this._itemsSection.addAction('Hybrid', () => {
-            if (currentProfile !== 'hybrid') {
-                Utility.switchHybrid(this.all_settings);
-                super.subtitle = 'Hybrid';
-                this.menu.setHeader('power-profile-performance-symbolic', headerTitle + ' → Hybrid', 'Restart to apply');
+        this._itemsSection.addAction('Hybrid' + (this.activeProfile === 'hybrid' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'hybrid' && this.requestedProfile === null) {
+                this.requestedProfile = 'hybrid';
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Hybrid mode...');
+                Utility.switchHybrid(this.all_settings, this._onSwitchComplete.bind(this));
             }
         });
-        this._itemsSection.addAction('Nvidia', () => {
-            if (currentProfile !== 'nvidia') {
-                Utility.switchNvidia(this.all_settings);
-                super.subtitle = 'Nvidia';
-                this.menu.setHeader('power-profile-performance-symbolic', headerTitle + ' → Nvidia', 'Restart to apply');
+        this._itemsSection.addAction('Nvidia'+ (this.activeProfile === 'nvidia' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'nvidia' && this.requestedProfile === null) {
+                this.requestedProfile = 'nvidia';
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Nvidia mode...');
+                Utility.switchNvidia(this.all_settings, this._onSwitchComplete.bind(this));
             }
         });
         this.menu.addMenuItem(this._itemsSection);
@@ -63,6 +65,25 @@ class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {
         // Ensure the settings are unavailable when the screen is locked
         settingsItem.visible = Main.sessionMode.allowSettings;
         this.menu._settingsActions[extensionObject.uuid] = settingsItem;
+    }
+
+    _onSwitchComplete() {
+        this.chosenProfile = Utility.getCurrentProfile();
+        if (!this.restartPending && (this.chosenProfile !== this.requestedProfile || this.activeProfile === this.requestedProfile)) {
+            super.subtitle = Utility.capitalizeFirstLetter(this.chosenProfile);
+            this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Choose a GPU mode');
+        }
+        else {
+            this.restartPending = true;
+            super.subtitle = Utility.capitalizeFirstLetter(this.chosenProfile) + '*';
+            this.menu.setHeader('power-profile-performance-symbolic', super.title, 
+                'Restart to apply ' + Utility.capitalizeFirstLetter(this.chosenProfile) + ' mode');
+            Utility.requestReboot();
+        }
+        
+        // Deal with the visuals of aborting while in restart pending scenario
+
+        this.requestedProfile = null;
     }
 });
 
