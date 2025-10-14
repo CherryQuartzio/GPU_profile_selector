71901badb22d557a6293f4fffe50200abb4942e9
Quang Nguyen <85609395+CherryQuartzio@users.noreply.github.com>
2025-10-14 07:45:15 -0700

    Fix custom argument support, add condition checking before profile switch, update UI (#30)
    * fix option flags compatibility and update UI
    
    * Reverse UI option ordering
    
    * prevent switching to the same mode
    
    * fix same profile switching prevention and update quick settings button
    
    * update icon, option description, and QS tile behavior
    
    * update icon, option description, and QS tile behavior
    
    * add .gitignore
    
    * Implemented condition checking for when envycontrol should not run
    
    * fix profile switching status display and add more description in extension settings
    
    * revert metadata file

--- ui/AttachedToBatteryView.js
+++ ui/AttachedToBatteryView.js
@@ -5,44 +5,53 @@ import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js'
 
 import * as Utility from '../lib/Utility.js';
 
-
 export const AttachedToBatteryToggle = GObject.registerClass(
-class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {
+class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {  
     _init(extensionObject) {
-        const currentProfile = Utility.getCurrentProfile();
+        this.activeProfile = Utility.getCurrentProfile(); // initialzied profile since startup
+        this.chosenProfile = this.activeProfile === Utility.GPU_PROFILE_UNKNOWN // currently selected profile
+                ? 'unknown'
+                : this.activeProfile;
+        this.restartPending = false;
+        this.doNotSwitch = false;
+        
         super._init({
-            title: currentProfile === Utility.GPU_PROFILE_UNKNOWN
-                ? 'GPU Profile'
-                : Utility.capitalizeFirstLetter(currentProfile),
-            iconName: 'selection-mode-symbolic',
+            title: 'GPU Profile',
+            subtitle: Utility.capitalizeFirstLetter(this.chosenProfile),
+            iconName: 'power-profile-performance-symbolic',
             toggleMode: false, // disable the possibility to click the button
-            checked: true,
+            checked: this.activeProfile === 'hybrid' || this.activeProfile === 'nvidia',
         });
         this.all_settings = extensionObject.getSettings();
 
-        const headerTitle = currentProfile === Utility.GPU_PROFILE_UNKNOWN
-            ? 'Select GPU Profile'
-            : Utility.capitalizeFirstLetter(currentProfile);
-
         // This function is unique to this class. It adds a nice header with an icon, title and optional subtitle.
-        this.menu.setHeader('selection-mode-symbolic', headerTitle, 'Choose a GPU mode');
+        this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Choose a GPU mode');
 
         // add a sections of items to the menu
         this._itemsSection = new PopupMenu.PopupMenuSection();
-        this._itemsSection.addAction('Nvidia', () => {
-            Utility.switchNvidia(this.all_settings);
-            super.title = 'Nvidia'
-            this.menu.setHeader('selection-mode-symbolic', 'Nvidia (Reboot needed)', 'Choose a GPU mode');
+        this._itemsSection.addAction('Integrated' + (this.activeProfile === 'integrated' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'integrated' && !this.doNotSwitch) {
+                this.doNotSwitch = true;
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Integrated mode...');
+                Utility.switchIntegrated(this._onSwitchComplete.bind(this));
+            }
         });
-        this._itemsSection.addAction('Hybrid', () => {
-            Utility.switchHybrid(this.all_settings);
-            super.title = 'Hybrid'
-            this.menu.setHeader('selection-mode-symbolic', 'Hybrid (Reboot needed)', 'Choose a GPU mode');
+        this._itemsSection.addAction('Hybrid' + (this.activeProfile === 'hybrid' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'hybrid' && !this.doNotSwitch) {
+                this.doNotSwitch = true;
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Hybrid mode...');
+                Utility.switchHybrid(this.all_settings, this._onSwitchComplete.bind(this));
+            }
         });
-        this._itemsSection.addAction('Integrated', () => {
-            Utility.switchIntegrated();
-            super.title = 'Integrated'
-            this.menu.setHeader('selection-mode-symbolic', 'Integrated (Reboot needed)', 'Choose a GPU mode');
+        this._itemsSection.addAction('Nvidia'+ (this.activeProfile === 'nvidia' ? ' (Active)' : ''), () => {
+            if (this.chosenProfile !== 'nvidia' && !this.doNotSwitch) {
+                this.doNotSwitch = true;
+                super.subtitle = 'Switching...';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Switching to Nvidia mode...');
+                Utility.switchNvidia(this.all_settings, this._onSwitchComplete.bind(this));
+            }
         });
         this.menu.addMenuItem(this._itemsSection);
 
@@ -57,6 +66,39 @@ class AttachedToBatteryToggle extends QuickSettings.QuickMenuToggle {
         settingsItem.visible = Main.sessionMode.allowSettings;
         this.menu._settingsActions[extensionObject.uuid] = settingsItem;
     }
+
+    _onSwitchComplete() {
+        // chosenProfile before switch
+        let priorProfile = this.chosenProfile;
+        this.chosenProfile = Utility.getCurrentProfile();
+
+        // if chosenProfile is the same as prior profile, operation aborted
+        if (this.chosenProfile === priorProfile) {
+            if (this.restartPending) {
+                super.subtitle = Utility.capitalizeFirstLetter(this.chosenProfile) + '*';
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 
+                    'Restart to apply ' + Utility.capitalizeFirstLetter(this.chosenProfile) + ' mode');
+            }
+            else { // GPU switch attempt aborted
+                super.subtitle = Utility.capitalizeFirstLetter(this.chosenProfile);
+                this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Choose a GPU mode');
+            }
+        }
+        else if (this.activeProfile === this.chosenProfile) {
+            super.subtitle = Utility.capitalizeFirstLetter(this.activeProfile);
+            this.menu.setHeader('power-profile-performance-symbolic', super.title, 'Choose a GPU mode');
+            this.restartPending = false;
+        }
+        else {
+            super.subtitle = Utility.capitalizeFirstLetter(this.chosenProfile) + '*';
+            this.menu.setHeader('power-profile-performance-symbolic', super.title, 
+                'Restart to apply ' + Utility.capitalizeFirstLetter(this.chosenProfile) + ' mode');
+            Utility.requestReboot();
+            this.restartPending = true;
+        }
+        
+        this.doNotSwitch = false;
+    }
 });
 
 export const AttachedToBatteryView = GObject.registerClass(
@@ -67,7 +109,7 @@ class AttachedToBatteryView extends QuickSettings.SystemIndicator {
 
     enable() {
         this._indicator = this._addIndicator();
-        this._indicator.icon_name = 'selection-mode-symbolic' //Gio.icon_new_for_string(Me.dir.get_path() + Utility.ICON_SELECTOR_FILE_NAME);
+        this._indicator.icon_name = 'power-profile-performance-symbolic' //Gio.icon_new_for_string(Me.dir.get_path() + Utility.ICON_SELECTOR_FILE_NAME);
         this._indicator.visible = false;
     }
 
